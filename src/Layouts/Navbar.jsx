8137a043b390d13dceda38e4b73f53318ce466db
Raktim Shrestha <raktim@rumsan.com>
2023-11-19 00:20:34 +0300

    voter page

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -15,8 +15,7 @@ import { Link, useNavigate, useLocation } from "react-router-dom";
 const user = {
   name: "Tom Cook",
   email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  imageUrl: "https://cdn-icons-png.flaticon.com/512/3607/3607444.png",
 };
 const navigation = [
   { name: "Dashboard", href: "/dashboard" },
--- src/pages/ChallengeDetail.jsx
+++ src/pages/ChallengeDetail.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function ChallengeDetail() {
   return <div>ChallengeDetail</div>;
 }
